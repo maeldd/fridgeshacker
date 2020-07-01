84b497095efe3969f62794d8bd4fb323fb9a8d9d
Mael donnard <mael.donnard@gmail.com>
2020-07-01 13:29:53 +0200

    add pg search to gemfile and model Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_ingredients,
+  against: [ :ingredients ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
